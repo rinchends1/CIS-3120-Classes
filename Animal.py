fbce2f60122187f3c957941a2df59e8e732fe748
rinchends1 <147345305+rinchends1@users.noreply.github.com>
2025-02-18 20:10:06 -0500

    assignment done

--- Animal.py
+++ Animal.py
@@ -1,7 +1,33 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
-
+    def __init__(self, name, species, age, color): 
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__color = color
+        print(f"Hello, I am {self.__name}, a {self.__species}.")
+    
     def talk(self):
-        print("hi")
+        print("Hi")
+    
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, new_name):
+        self.__name = new_name
+        print(f"My new name is {self.__name}")
+    
+    def get_species(self):
+        return self.__species
+    
+    def get_age(self):
+        return self.__age
+    
+    def get_color(self): 
+        return self.__color
+    
+    def birthday(self):
+        self.__age += 1
+        print(f"Happy Birthday! I am now {self.__age} years old, and {self.__color} in color")
+
+
 
